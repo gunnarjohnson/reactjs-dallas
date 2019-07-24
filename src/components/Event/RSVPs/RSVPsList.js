889de84350317ee9683eb93617ee3343ecf15e2d
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2019-07-24 17:33:24 -0500

    Add error handling for RSVPs

--- src/components/Event/RSVPs/RSVPsList.js
+++ src/components/Event/RSVPs/RSVPsList.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 const RSVPsList = (props) => (
   <ul className="rsvps-list">
-    {props.rsvps.map((rsvp, i) => (
-      rsvp.response === 'yes' && (
-        <li className="rsvps-list__item" key={i}>
-          {rsvp.member.name}
-          {rsvp.guests > 0 && ' + ' + rsvp.guests}
-        </li>
-    )))}
+    {!props.rsvps.errors ? (
+      props.rsvps.map((rsvp, i) => (
+        rsvp.response === 'yes' && (
+          <li className="rsvps-list__item" key={i}>
+            {rsvp.member.name}
+            {rsvp.guests > 0 && ' + ' + rsvp.guests}
+          </li>
+        )
+      ))
+    ) : (
+      <li className="rsvps-list__item">RSVPs not available</li>
+    )}
   </ul>
 );
 
